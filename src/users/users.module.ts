24432e71c3b9e3f92a7cd1ab7128eeeaf0ce4d68
Kiet Huynh <kiet.huynh@mercatus.com>
2024-07-23 21:20:56 +0700

    update estimate function

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,19 +4,15 @@ import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor,
-    },
-  ],
+  providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
